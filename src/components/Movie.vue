2b4eb3e0360ae055b77ae028cdda08b63dadc591
Elliot Burin <eburin2@gmail.com>
2019-04-17 14:33:53 -0700

    Update Title in Movies Component

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper">
-    <h3>MoviesDB Search</h3>
+    <h3>MoviesDB API</h3>
     <a target="_blank" href="https://eburin2.github.io/React-Movie-Database-API/">
       <img class="img-responsive img-main" src="https://s3-us-west-2.amazonaws.com/elliotburin.com/movie.png"/>
     </a>
